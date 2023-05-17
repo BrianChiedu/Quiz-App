89142762d5c009d2e5d0d4c616bcae53a880c6f8
Brian Chukwuisiocha <brianchukwuisiocha@gmail.com>
2023-05-16 23:05:48 -0400

    First commit

---
+++ Quizzler-iOS13/Model/Question.swift
@@ -0,0 +1,21 @@
+//
+//  Question.swift
+//  Quizzler
+//
+//  Created by Brian Chukwuisiocha .
+//
+//
+
+import Foundation
+
+struct Question {
+    let question: String
+    let answer: String
+    
+    init(q: String, a: String) {
+        question = q
+        answer = a
+    }
+}
+
+    
